d99fc456184d87d377c2e6a38b3f3b02b31f755e
Wu Xinfeng <390155409@qq.com>
2022-10-25 17:03:58 +0000

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w"></div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.w{
+  height: 100px;
+  width: 100px;
+  background-color: #535bf2;
+}
+
+</style>
